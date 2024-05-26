a8fbae9d35442cd581f6cc9e810417b55aa4da91
Kaeneth Dayao <kendayao@gmail.com>
2024-05-25 21:57:37 -0700

    styles megamillion page

--- src/routes/MegaMillion.js
+++ src/routes/MegaMillion.js
@@ -6,12 +6,15 @@ function MegaMillion(){
     return(
         <div className='megamillion'>
             <div className='megamillion__container'>
-                <p>content</p>
-                <p>content</p>
-                <p>content</p>
-                <p>content</p>
-                <p>content</p>
-                <p>content</p>
+                <img className='megamillion__image'src='https://mnlottery-craft-assets-prod.s3.us-gov-west-1.amazonaws.com/Clockwork-Images/megamillionslogo.png' alt='megamillion logo' />
+                <div className='megamillion__body'>
+                    <p>46</p>
+                    <p>54</p>
+                    <p>56</p>
+                    <p>67</p>
+                    <p>70</p>
+                    <p>16</p>
+                </div>
             </div>
         </div>
     )
