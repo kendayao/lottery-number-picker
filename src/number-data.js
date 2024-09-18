1abab6c67fe5b78617703becf23c1715479f6517
Kaeneth Dayao <kendayao@gmail.com>
2024-09-17 21:24:55 -0700

    number data

--- src/number-data.js
+++ src/number-data.js
@@ -1,5 +1,88 @@
-export const megaMillionNumbersData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
-export const megaNumbersData = [1,2,3,4,5,6,7,8]
+export const megaMillionNumbersData = [
+    66,
+    3,
+    29,
+    61,
+    10,
+    18,
+    33,
+    12,
+    15,
+    19,
+    22,
+    26,
+    23,
+    30,
+    46,
+    54,
+    55,
+    57,
+    2,
+    6,
+    8,
+    40,
+    42,
+    44,
+    50,
+]
 
-export const powerBallNumbersData = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
-export const powerNumbersData = [1,2,3,4,5,6,7,8,9,10]
+
+export const megaNumbersData = [
+11,
+18,
+24,
+4,
+13,
+25,
+7,
+1,
+3,
+12
+]
+
+
+
+export const powerBallNumbersData = [
+
+    24,
+    27,
+    33,
+    9,
+    47,
+    19,
+    38,
+    4,
+    64,
+    68,
+    69,
+    12,
+    21,
+    31,
+    36,
+    66,
+    11,
+    2,
+    39,
+    44,
+    67,
+    23,
+    26,
+    34,
+    35
+]
+
+
+export const powerNumbersData = [
+24,
+9,
+19,
+4,
+12,
+21,
+11,
+2,
+23,
+26,
+]
+
+
