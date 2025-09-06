616b744a88db779218749a1bf468c628879f70f2
Kaeneth Dayao <kendayao@gmail.com>
2025-09-06 07:56:40 -0700

    fix typo on how to play file

--- src/routes/Howtoplay.js
+++ src/routes/Howtoplay.js
@@ -8,7 +8,7 @@ function Howtoplay(){
                 <h1>How to Play</h1>
                 <p>Lottery Number picker app generates a combination of numbers that are the top 20 most common numbers and top 10 power/mega number in each megamillion and poweball game </p>
                 <h3>MegaMillion</h3>
-                <p>For the megamillion game, the lottery number picker app will select five numbers and one mega number. The mega number will be the yellow ball </p>
+                <p>For the megamillion game, the lottery number picker app will select five numbers and one mega number. The mega number will be the yellow ball. </p>
                 <div className='howtoplay__image'>
                     <div></div>
                     <div></div>
@@ -18,7 +18,7 @@ function Howtoplay(){
                     <div className='howtoplay__mega'></div>
                 </div>
                 <h3>Powerball</h3>
-                <p>For the powerball game, the lottery number picker app will select five numbers and one power number. The power number will be the red ball</p>
+                <p>For the powerball game, the lottery number picker app will select five numbers and one power number. The power number will be the red ball.</p>
                 <div className='howtoplay__image'>
                     <div></div>
                     <div></div>
